348a468823607d10d25312b5067d596f19ee3df4
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2023-05-13 11:44:22 +0100

    update contract

--- hooks/useConnect.tsx
+++ hooks/useConnect.tsx
@@ -3,93 +3,86 @@ import { useEffect } from "react";
 import { useUserContext } from "../context/UserContext";
 import { ethers } from "ethers";
 import { useContractContext } from "../context/ContractContext";
+import { mumbaiNFTAddress } from "lib/contracts";
+import NFTabi from "lib/contracts/abi/ABI.json";
 
 const useConnect = () => {
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-  const wallets = useWallets();
-  const [{ provider, signer }, userDispatch] = useUserContext();
-  const [_, dispatch] = useContractContext();
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+    const wallets = useWallets();
+    const [{ provider, signer }, userDispatch] = useUserContext();
+    const [_, dispatch] = useContractContext();
 
-  useEffect(() => {
-    if (wallet) {
-      const { provider } = wallet;
-      userDispatch({
-        type: "UPDATE_ADDRESS",
-        address: wallet.accounts[0].address,
-      });
-      userDispatch({
-        type: "UPDATE_PROVIDER",
-        provider: new ethers.providers.Web3Provider(provider),
-      });
-    }
-  }, [wallet]);
+    useEffect(() => {
+        if (wallet) {
+            const { provider } = wallet;
+            userDispatch({
+                type: "UPDATE_ADDRESS",
+                address: wallet.accounts[0].address,
+            });
+            userDispatch({
+                type: "UPDATE_PROVIDER",
+                provider: new ethers.providers.Web3Provider(provider),
+            });
+        }
+    }, [wallet]);
 
-  useEffect(() => {
-    if (provider) {
-      userDispatch({
-        type: "UPDATE_SIGNER",
-        signer: provider.getSigner(),
-      });
-    }
-  }, [provider]);
+    useEffect(() => {
+        if (provider) {
+            userDispatch({
+                type: "UPDATE_SIGNER",
+                signer: provider.getSigner(),
+            });
+        }
+    }, [provider]);
 
-  const handleConnect = () => {
-    if (!wallet && typeof window !== "undefined") {
-      connect();
-    } else if (!connecting && wallet) {
-      disconnect({ label: wallet.label });
+    const handleConnect = () => {
+        if (!wallet && typeof window !== "undefined") {
+            connect();
+        } else if (!connecting && wallet) {
+            disconnect({ label: wallet.label });
 
-      userDispatch({
-        type: "UPDATE_ADDRESS",
-        address: "",
-      });
-    }
-  };
+            userDispatch({
+                type: "UPDATE_ADDRESS",
+                address: "",
+            });
+        }
+    };
 
-  useEffect(() => {
-    if (provider || signer) {
-      //   dispatch({
-      //     type: "UPDATE_DIAMOND",
-      //     diamond: new ethers.Contract(
-      //       GT_DIAMOND_ADDRESS,
-      //       diamondAbi,
-      //       signer || provider
-      //     ),
-      //   });
-    }
-  }, [provider, signer]);
+    useEffect(() => {
+        if (provider || signer) {
+            dispatch({
+                type: "UPDATE_DIAMOND",
+                NFTcontract: new ethers.Contract(mumbaiNFTAddress, NFTabi, signer || provider),
+            });
+        }
+    }, [provider, signer]);
 
-  useEffect(() => {
-    const walletData = window.localStorage.getItem(
-      "galaxyThroneConnectedWallets"
-    );
-    if (walletData) {
-      const pastConnectedWallets = JSON.parse(walletData);
-      if (pastConnectedWallets?.length > 0)
-        connect({
-          autoSelect: { label: pastConnectedWallets[0], disableModals: true },
-        });
-    }
-  }, []);
+    useEffect(() => {
+        const walletData = window.localStorage.getItem("galaxyThroneConnectedWallets");
+        if (walletData) {
+            const pastConnectedWallets = JSON.parse(walletData);
+            if (pastConnectedWallets?.length > 0)
+                connect({
+                    autoSelect: { label: pastConnectedWallets[0], disableModals: true },
+                });
+        }
+    }, []);
 
-  useEffect(() => {
-    if (wallets) {
-      let connected: string[] = [];
-      if (wallets?.length > 0) {
-        connected = wallets.map(({ label }) => label);
-      }
-      window.localStorage.setItem(
-        "galaxyThroneConnectedWallets",
-        JSON.stringify(connected)
-      );
-    }
-  }, [wallets]);
+    useEffect(() => {
+        if (wallets) {
+            let connected: string[] = [];
+            if (wallets?.length > 0) {
+                connected = wallets.map(({ label }) => label);
+            }
+            window.localStorage.setItem("galaxyThroneConnectedWallets", JSON.stringify(connected));
+        }
+    }, [wallets]);
 
-  return {
-    wallet,
-    connecting,
-    handleConnect,
-  };
+    return {
+        wallet,
+        connecting,
+        handleConnect,
+    };
 };
 
 export default useConnect;
---
+++ lib/contracts/index.ts
@@ -0,0 +1,1 @@
+export const mumbaiNFTAddress = "0xf8E18Ec76fE7b6f0912E93F2bc418113e0aCF76e";
