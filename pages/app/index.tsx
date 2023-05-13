aac5e955f25b6ca97591e14376cddb88e925c0e2
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2023-05-13 14:36:58 +0100

    create app page

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,5 +1,12 @@
+import AppHero from "components/App/Hero";
+import { benefitTwo } from "lib/data";
+
 const AppHome = () => {
-  return <div></div>;
+    return (
+        <div>
+            <AppHero imgPos="right" data={benefitTwo} />
+        </div>
+    );
 };
 
 export default AppHome;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,14 @@
 import Head from "next/head";
-import { benefitOne, benefitTwo } from "../lib/data";
+import { benefitOne } from "../lib/data";
 import Hero from "components/LandingPage/Hero";
 import SectionTitle from "components/LandingPage/SectionTitle";
-import Benefits from "components/LandingPage/Benefits";
 import Video from "components/LandingPage/Video";
 import Testimonials from "components/LandingPage/Testimonials";
 import Faq from "components/LandingPage/Faq";
 import Cta from "components/LandingPage/Cta";
-import lensLogo from "../public/img/logos/lens-logo.jpeg";
-import worldCoinLogo from "../public/img/logos/worldcoin-logo.jpeg";
-import polygonLogo from "../public/img/logos/polygon-logo.png";
-import Modal from "components/_shared/Modal";
-import { useState } from "react";
+import Benefits from "components/LandingPage/Benefits";
 
 const Home = () => {
-    const [openModal, setOpenModal] = useState<boolean>(true);
-
-    const buttons = [
-        { label: "Lens", img: lensLogo, color: "text-green-600" },
-        { label: "WorldCoin", img: worldCoinLogo, color: "text-black" },
-        { label: "PolygonId", img: polygonLogo, color: "text-[#7823BA]" },
-        { label: "No ID", color: "bg-gray-900 text-white" },
-    ];
-
     return (
         <>
             <Head>
@@ -30,17 +16,12 @@ const Home = () => {
                 <meta name="description" content="Monuments Men is a free landing page template built with next.js & Tailwind CSS" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="cursor-pointer text-2xl text-red-200" onClick={() => setOpenModal(true)}>
-                Open Modal example
-            </div>
-            <Modal buttons={buttons} openModal={openModal} closeModal={() => setOpenModal(false)} />
             <Hero />
             <SectionTitle pretitle="Monuments Men Benefits" title=" Why should you use this landing page">
                 Monuments Men is a free landing page & marketing website template for startups and indie projects. Its built with Next.js &
                 TailwindCSS. And its completely open-source.
             </SectionTitle>
             <Benefits data={benefitOne} />
-            <Benefits imgPos="right" data={benefitTwo} />
             <SectionTitle pretitle="Watch a video" title="Learn how to fullfil your needs">
                 This section is to highlight a promo or demo video of your product. Analysts says a landing page with video has 3% more conversion
                 rate. So, don&apos;t forget to add one. Just like this.
